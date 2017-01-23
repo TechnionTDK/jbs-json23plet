2eab882a4ac9749ae502c472c6b5cb188a0b0143
Yehonatan <yon_b@campus.technion.ac.il>
2017-01-23 21:15:46 +0200

    add rules to RdfTypeGenerator

--- src/main/java/json23plet/generators/regexGenerators/RdfTypeGenerator.java
+++ src/main/java/json23plet/generators/regexGenerators/RdfTypeGenerator.java
@@ -3,16 +3,18 @@ package json23plet.generators.regexGenerators;
 import json23plet.JsonValidators.JsonValidator;
 import json23plet.JsonValidators.OntologyValidator;
 import json23plet.modules.Json;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.rdf.model.Resource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static json23plet.modules.Json.json;
 import static json23plet.modules.Regex.regex;
 import static json23plet.modules.Triplet.triplet;
 import static json23plet.ontologies.BaseOntology.OWL_C_THING;
 import static json23plet.ontologies.BaseOntology.RDF_P_TYPE;
-import static json23plet.ontologies.JbsOntology.JBO_C_PERUSHTANACH;
-import static json23plet.ontologies.JbsOntology.JBO_C_TORAH;
+import static json23plet.ontologies.JbsOntology.*;
 
 /**
  * Created by yon_b on 02/01/17.
@@ -20,7 +22,7 @@ import static json23plet.ontologies.JbsOntology.JBO_C_TORAH;
 public class RdfTypeGenerator extends BaseRegexGenerator {
     @Override
     public void registerGenerators() {
-        registerGenerator(new IRegExGenerator() {
+        registerGenerator(new IRegExGenerator() { //Thing type
             @Override
             public void generate(Json js) {
                 triplet()
@@ -31,24 +33,24 @@ public class RdfTypeGenerator extends BaseRegexGenerator {
 
             @Override
             public boolean match(Json json) {
-                return regex("jbr:(.*)")
+                return regex("jbr:.*")
                         .match(json.value("uri"));
             }
 
         });
-        registerGenerator(new IRegExGenerator() {
+        registerGenerator(new IRegExGenerator() { // Tanach type
             @Override
             public void generate(Json js) {
                 triplet()
                         .subject(js.value("uri"))
                         .predicate(RDF_P_TYPE)
-                        .object(JBO_C_PERUSHTANACH);
+                        .object(JBO_C_TANACH);
             }
 
             @Override
             public boolean match(Json json) {
                 try {
-                    return regex("jbr:tanach-(.*)-(\\d+)-(\\d+)-(\\d+)")
+                    return regex("jbr:tanach-.*") //-(.*)-(\d+)-(\d+)-(\d+)
                             .match(json.value("uri"));
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -57,6 +59,180 @@ public class RdfTypeGenerator extends BaseRegexGenerator {
             }
         });
 
+        registerGenerator(new IRegExGenerator() { // Torah type
+            @Override
+            public void generate(Json js) {
+                triplet()
+                        .subject(js.value("uri"))
+                        .predicate(RDF_P_TYPE)
+                        .object(JBO_C_TORAH);
+            }
+
+            @Override
+            public boolean match(Json json) {
+                try {
+                    return regex("jbr:tanach-[1-5]-\\d+-\\d+")
+                            .match(json.value("uri"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
+
+        registerGenerator(new IRegExGenerator() { // Neviyim type
+            @Override
+            public void generate(Json js) {
+                triplet()
+                        .subject(js.value("uri"))
+                        .predicate(RDF_P_TYPE)
+                        .object(JBO_C_NEVIYIM);
+            }
+
+            @Override
+            public boolean match(Json json) {
+                try {
+                    return regex("jbr:tanach-[6-9]-\\d+-\\d+" +
+                            "|jbr:tanach-1[0-9]-\\d+-\\d+" +
+                            "|jbr:tanach-2[0-6]-\\d+-\\d+")
+                            .match(json.value("uri"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
+
+        registerGenerator(new IRegExGenerator() { // Ketuvim type
+            @Override
+            public void generate(Json js) {
+                triplet()
+                        .subject(js.value("uri"))
+                        .predicate(RDF_P_TYPE)
+                        .object(JBO_C_KETUVIM);
+            }
+
+            @Override
+            public boolean match(Json json) {
+                try {
+                    return regex("jbr:tanach-2[7-9]-\\d+-\\d+|jbr:tanach-3[0-9]-\\d+-\\d+")
+                            .match(json.value("uri"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
+
+
+        registerGenerator(new IRegExGenerator() { // sefer type
+            String seferPosition ="";
+
+            public void generate(Json js) {
+                for (OntClass c : model.listClasses().toList()) {
+                    if (c.hasProperty(JBO_P_POSITION)
+                            && c.hasSuperClass(JBO_C_TANACH)
+                            && c.getProperty(JBO_P_POSITION).getObject().toString().equals(seferPosition)) {
+                        triplet()
+                                .subject(js.value("uri"))
+                                .predicate(RDF_P_TYPE)
+                                .object(c);
+                        return;
+                    }
+                }
+
+            }
+
+            @Override
+            public boolean match(Json json) {
+                if ( json.value("uri").startsWith("jbr:tanach-")) {
+                    seferPosition = json.value("uri").split("-")[1];
+                }
+                try {
+                    return regex("jbr:tanach-\\d+-\\d+-\\d+|jbr:tanach-3[0-9]-\\d+-\\d+")
+                            .match(json.value("uri"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
+
+        registerGenerator(new IRegExGenerator() { // mishneTorah type
+            @Override
+            public void generate(Json js) {
+                triplet()
+                        .subject(js.value("uri"))
+                        .predicate(RDF_P_TYPE)
+                        .object(JBO_C_MISHNETORAH);
+                triplet()
+                        .subject(js.value("uri"))
+                        .predicate(RDF_P_TYPE)
+                        .object(JBO_C_SEFERHAMITZVOT);
+            }
+
+            @Override
+            public boolean match(Json json) {
+                try {
+                    return regex("jbr:seferhamitzvot-\\d+-\\d+")
+                            .match(json.value("uri"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
+
+
+        registerGenerator(new IRegExGenerator() { // shmonaKavatzim type
+            @Override
+            public void generate(Json js) {
+                triplet()
+                        .subject(js.value("uri"))
+                        .predicate(RDF_P_TYPE)
+                        .object(JBO_C_SIFREYRAVKUK);
+                triplet()
+                        .subject(js.value("uri"))
+                        .predicate(RDF_P_TYPE)
+                        .object(JBO_C_SHEMONAKEVATZIM);
+            }
+
+            @Override
+            public boolean match(Json json) {
+                try {
+                    return regex("jbr:shmonakvatzim-.*")
+                            .match(json.value("uri"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
+
+        registerGenerator(new IRegExGenerator() { // mesilatYesharim type
+            @Override
+            public void generate(Json js) {
+                triplet()
+                        .subject(js.value("uri"))
+                        .predicate(RDF_P_TYPE)
+                        .object(JBO_C_MESILATYESHARIM);
+                triplet()
+                        .subject(js.value("uri"))
+                        .predicate(RDF_P_TYPE)
+                        .object(JBO_C_SIFREYRAMCHAL);
+            }
+
+            @Override
+            public boolean match(Json json) {
+                try {
+                    return regex("jbr:mesilatyesharim-.*")
+                            .match(json.value("uri"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
     }
 
     @Override
