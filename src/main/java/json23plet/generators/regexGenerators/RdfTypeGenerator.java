eb93aaef3d2a6c70210ac36ffd77c6e71b53143e
Yehonatan <yon_b@campus.technion.ac.il>
2017-01-24 19:06:43 +0200

    update RdfTypeGenerator

--- src/main/java/json23plet/generators/regexGenerators/RdfTypeGenerator.java
+++ src/main/java/json23plet/generators/regexGenerators/RdfTypeGenerator.java
@@ -49,13 +49,8 @@ public class RdfTypeGenerator extends BaseRegexGenerator {
 
             @Override
             public boolean match(Json json) {
-                try {
-                    return regex("jbr:tanach-.*") //-(.*)-(\d+)-(\d+)-(\d+)
-                            .match(json.value("uri"));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return false;
+                return regex("jbr:tanach-.*") //-(.*)-(\d+)-(\d+)-(\d+)
+                        .match(json.value("uri"));
             }
         });
 
@@ -70,13 +65,8 @@ public class RdfTypeGenerator extends BaseRegexGenerator {
 
             @Override
             public boolean match(Json json) {
-                try {
-                    return regex("jbr:tanach-[1-5]-\\d+-\\d+")
-                            .match(json.value("uri"));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return false;
+                return regex("jbr:tanach-[1-5]-\\d+-\\d+")
+                        .match(json.value("uri"));
             }
         });
 
@@ -91,15 +81,10 @@ public class RdfTypeGenerator extends BaseRegexGenerator {
 
             @Override
             public boolean match(Json json) {
-                try {
-                    return regex("jbr:tanach-[6-9]-\\d+-\\d+" +
-                            "|jbr:tanach-1[0-9]-\\d+-\\d+" +
-                            "|jbr:tanach-2[0-6]-\\d+-\\d+")
-                            .match(json.value("uri"));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return false;
+                return regex("jbr:tanach-[6-9]-\\d+-\\d+" +
+                        "|jbr:tanach-1[0-9]-\\d+-\\d+" +
+                        "|jbr:tanach-2[0-6]-\\d+-\\d+")
+                        .match(json.value("uri"));
             }
         });
 
@@ -114,13 +99,8 @@ public class RdfTypeGenerator extends BaseRegexGenerator {
 
             @Override
             public boolean match(Json json) {
-                try {
-                    return regex("jbr:tanach-2[7-9]-\\d+-\\d+|jbr:tanach-3[0-9]-\\d+-\\d+")
-                            .match(json.value("uri"));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return false;
+                return regex("jbr:tanach-2[7-9]-\\d+-\\d+|jbr:tanach-3[0-9]-\\d+-\\d+")
+                        .match(json.value("uri"));
             }
         });
 
@@ -128,11 +108,16 @@ public class RdfTypeGenerator extends BaseRegexGenerator {
         registerGenerator(new IRegExGenerator() { // sefer type
             String seferPosition ="";
 
+            /*
+                In JbsOntology there is 39 different books.
+                Instead of writing a rule to each book, we iterating over the JboOntology using JboOntology.model
+                classes and use its jbo:position predicate to map between the json uri and the book name
+             */
             public void generate(Json js) {
-                for (OntClass c : model.listClasses().toList()) {
-                    if (c.hasProperty(JBO_P_POSITION)
-                            && c.hasSuperClass(JBO_C_TANACH)
-                            && c.getProperty(JBO_P_POSITION).getObject().toString().equals(seferPosition)) {
+                for (OntClass c : model.listClasses().toList()) { // c holds the current class such as "Bereshit"
+                    if (c.hasProperty(JBO_P_POSITION) //  c has property of jbo:position
+                            && c.hasSuperClass(JBO_C_TANACH) // c has superclass of jbo:Tanach
+                            && c.getProperty(JBO_P_POSITION).getObject().toString().equals(seferPosition)) { // c.position equals to the current uri position
                         triplet()
                                 .subject(js.value("uri"))
                                 .predicate(RDF_P_TYPE)
@@ -148,13 +133,8 @@ public class RdfTypeGenerator extends BaseRegexGenerator {
                 if ( json.value("uri").startsWith("jbr:tanach-")) {
                     seferPosition = json.value("uri").split("-")[1];
                 }
-                try {
-                    return regex("jbr:tanach-\\d+-\\d+-\\d+|jbr:tanach-3[0-9]-\\d+-\\d+")
-                            .match(json.value("uri"));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return false;
+                return regex("jbr:tanach-\\d+-\\d+-\\d+|jbr:tanach-3[0-9]-\\d+-\\d+")
+                        .match(json.value("uri"));
             }
         });
 
@@ -173,13 +153,8 @@ public class RdfTypeGenerator extends BaseRegexGenerator {
 
             @Override
             public boolean match(Json json) {
-                try {
-                    return regex("jbr:seferhamitzvot-\\d+-\\d+")
-                            .match(json.value("uri"));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return false;
+                return regex("jbr:seferhamitzvot-\\d+-\\d+")
+                        .match(json.value("uri"));
             }
         });
 
@@ -199,13 +174,8 @@ public class RdfTypeGenerator extends BaseRegexGenerator {
 
             @Override
             public boolean match(Json json) {
-                try {
-                    return regex("jbr:shmonakvatzim-.*")
-                            .match(json.value("uri"));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return false;
+                return regex("jbr:shmonakvatzim-.*")
+                        .match(json.value("uri"));
             }
         });
 
@@ -224,13 +194,8 @@ public class RdfTypeGenerator extends BaseRegexGenerator {
 
             @Override
             public boolean match(Json json) {
-                try {
-                    return regex("jbr:mesilatyesharim-.*")
-                            .match(json.value("uri"));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return false;
+                return regex("jbr:mesilatyesharim-.*")
+                        .match(json.value("uri"));
             }
         });
     }
