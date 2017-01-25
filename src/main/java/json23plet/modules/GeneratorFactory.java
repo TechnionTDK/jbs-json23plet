f8816bc9606dd96f442f7bac8b909b7350f0e57b
Yehonatan <yon_b@campus.technion.ac.il>
2017-01-25 17:32:23 +0200

    typo fix

--- src/main/java/json23plet/modules/GeneratorFactory.java
+++ src/main/java/json23plet/modules/GeneratorFactory.java
@@ -20,7 +20,7 @@ import java.util.Map;
 public class GeneratorFactory {
 
     static public void generate(String gen, String jsonRoot, String outputDirRoot) throws IOException {
-        System.out.println("Activating " + gen + " on " + jsonRoot + "directory...");
+        System.out.println("Activating " + gen + " on " + jsonRoot + " directory...");
         int totalWork = (int) Files.find(Paths.get(jsonRoot), 999, (p, bfa) -> bfa.isRegularFile()).count();
         ProgressBar pb = new ProgressBar(totalWork);
         Files.find(Paths.get(jsonRoot), 999, (p, bfa) -> bfa.isRegularFile()).forEach(file -> {
